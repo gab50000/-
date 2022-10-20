a01ee65c2e1f7d2d802ecd917d563b6ea69a6c0a
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-10-20 21:27:23 +0200

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::io;
+
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
-fn main() {
-    day01::solve_b();
-    day02::solve_a();
-    day02::solve_b();
-    day03::solve_a();
-    day03::solve_b();
+fn main() -> io::Result<()> {
+    day01::solve_a()?;
+    day01::solve_b()?;
+    day02::solve_a()?;
+    day02::solve_b()?;
+    day03::solve_a()?;
+    day03::solve_b()?;
+    day04::solve_a()?;
+    Ok(())
 }
