1740a62170ef05b0f18c6a941f824a5941a7c394
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-10-22 19:53:03 +0200

    Implement passport

--- src/day04.rs
+++ src/day04.rs
@@ -1,13 +1,18 @@
 use std::{io, str::FromStr};
 
 use nom::{
+    branch::{alt, permutation},
     bytes::complete::tag,
     character::complete::char,
-    character::complete::{alpha1, digit1},
+    character::complete::{alphanumeric1, digit1, line_ending, space0},
     combinator::{map, map_res},
-    sequence::{preceded, tuple},
+    sequence::{preceded, terminated, tuple},
     IResult,
 };
+
+// fn le_parser(input: &str) -> IResult<&str, &str> {
+//     line_ending(input)
+// }
 
 macro_rules! make_struct {
     ($id:ident, &str, $tag:literal) => {
@@ -16,7 +21,8 @@ macro_rules! make_struct {
         impl<'a> $id<'a> {
             fn parse(input: &'a str) -> IResult<&'a str, Self> {
                 let prefix = tuple((tag($tag), char(':')));
-                let parser = preceded(prefix, alpha1);
+                let space_or_newline = alt((space0, line_ending));
+                let parser = terminated(preceded(prefix, alphanumeric1), space_or_newline);
                 let mut p = map(parser, Self);
                 p(input)
             }
@@ -29,7 +35,8 @@ macro_rules! make_struct {
             fn parse(input: &str) -> IResult<&str, Self> {
                 let prefix = tuple((tag($tag), char(':')));
                 let parse_numbers = map_res(digit1, <$type>::from_str);
-                let parser = preceded(prefix, parse_numbers);
+                let space_or_newline = alt((space0, line_ending));
+                let parser = terminated(preceded(prefix, parse_numbers), space_or_newline);
                 let mut p = map(parser, Self);
                 p(input)
             }
@@ -53,17 +60,78 @@ fn test_birth_year() {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct Passport<'a> {
-    byr: BirthYear,
-    iyr: IssueYear,
-    eyr: ExpirationYear,
-    hgt: Height,
-    hcl: HairColor<'a>,
-    ecl: EyeColor<'a>,
-    pid: PassportID,
-    cid: CountryID,
+    pub byr: Option<BirthYear>,
+    pub iyr: Option<IssueYear>,
+    pub eyr: Option<ExpirationYear>,
+    pub hgt: Option<Height>,
+    pub hcl: Option<HairColor<'a>>,
+    pub ecl: Option<EyeColor<'a>>,
+    pub pid: Option<PassportID>,
+    pub cid: Option<CountryID>,
+}
+
+impl<'a> Passport<'a> {
+    fn new() -> Self {
+        Self {
+            byr: None,
+            iyr: None,
+            eyr: None,
+            hgt: None,
+            hcl: None,
+            ecl: None,
+            pid: None,
+            cid: None,
+        }
+    }
+
+    fn parse(input: &'a str) -> Self {
+        let mut p = permutation((
+            BirthYear::parse,
+            IssueYear::parse,
+            ExpirationYear::parse,
+            Height::parse,
+            HairColor::parse,
+            EyeColor::parse,
+            PassportID::parse,
+            CountryID::parse,
+        ));
+        let result = p(input);
+
+        let mut passport = Passport::new();
+        if let Ok((_, values)) = result {
+            passport.byr = Some(values.0);
+            passport.iyr = Some(values.1);
+            passport.eyr = Some(values.2);
+            passport.hgt = Some(values.3);
+            passport.hcl = Some(values.4);
+            passport.ecl = Some(values.5);
+            passport.pid = Some(values.6);
+            passport.cid = Some(values.7);
+        }
+        passport
+    }
+
+    fn is_valid(&self) -> bool {
+        self.byr.is_some()
+            && self.iyr.is_some()
+            && self.eyr.is_some()
+            && self.hgt.is_some()
+            && self.hcl.is_some()
+            && self.ecl.is_some()
+            && self.pid.is_some()
+    }
+}
+
+#[test]
+fn test_passport() {
+    let p = Passport::new();
+    assert!(!p.is_valid());
 }
 
 pub fn solve_a() -> io::Result<()> {
-    println!("{:?}", CountryID(123));
+    let p =
+        Passport::parse("hgt:172 pid:170 hcl:17106b iyr:2012 ecl:gry cid:123 eyr:2020 byr:1990");
+    println!("{:?}", p);
+    // Passport::parse("hgt:172in pid:170cm hcl:17106b iyr:2012 ecl:gry cid:123 eyr:2020 byr:1990");
     Ok(())
 }
